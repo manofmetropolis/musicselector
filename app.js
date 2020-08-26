80f43b3be6b48dda6f336b6658da18c5e2ab8745
R Graff <rmgraff@gmail.com>
2020-08-26 18:13:44 -0500

    Created albums array

---
+++ app.js
@@ -0,0 +1,22 @@
+const albumsList = [
+    {
+        "artist": "Prince",
+        "title": "Under the Cherry Moon",
+        "releaseDate": 1986
+    },
+    {
+        "artist": "Lauryn Hill",
+        "title": "The Miseducation of Lauryn Hill",
+        "releaseDate": 1998
+    },
+    {
+        "artist": "Donna Summer",
+        "title": "The Miseducation of Lauryn Hill",
+        "releaseDate": 1979
+    },
+    {
+        "artist": "Queen",
+        "title": "Sheer Heart Attack",
+        "releaseDate": 1973
+    }
+];
