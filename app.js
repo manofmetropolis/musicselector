cd916bafc97e2c2c1dc1f01d54ffe1b83f6bb991
R Graff <rmgraff@gmail.com>
2020-08-26 20:15:55 -0500

    Added album information and variables to JS

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
+const resultsBox = document.getElementById("resultsBox");
+const chooseButton = document.getElementById("chooseButton");
+const albumInfo = document.getElementById("albumInfo");
+const albumTitle = document.getElementById("albumTitle");
+const albumCover = document.getElementById("albumCover");
+
 const albumsList = [
     {
         "artist": "Prince",
         "title": "Under the Cherry Moon",
-        "releaseDate": 1986
+        "releaseDate": 1986,
+        "cover": "https://images-na.ssl-images-amazon.com/images/I/81BW-AbIfSL._SL1500_.jpg"
     },
     {
         "artist": "Lauryn Hill",
         "title": "The Miseducation of Lauryn Hill",
-        "releaseDate": 1998
+        "releaseDate": 1998,
+        "cover": "https://images-na.ssl-images-amazon.com/images/I/81CqPy68mWL._SL1500_.jpg"
     },
     {
         "artist": "Donna Summer",
-        "title": "The Miseducation of Lauryn Hill",
-        "releaseDate": 1979
+        "title": "Bad Girls",
+        "releaseDate": 1979,
+        "cover": "https://images-na.ssl-images-amazon.com/images/I/71EWJmd0VLL._SL1200_.jpg"
     },
     {
         "artist": "Queen",
         "title": "Sheer Heart Attack",
-        "releaseDate": 1973
+        "releaseDate": 1973,
+        "cover": "https://images-na.ssl-images-amazon.com/images/I/91k9o225K6L._SL1500_.jpg"
+    },
+    {
+        "artist": "Fleetwood Mac",
+        "title": "Rumours",
+        "releaseDate": 1977,
+        "cover": "https://images-na.ssl-images-amazon.com/images/I/71HWqbh0BLL._SL1425_.jpg"
+    },
+    {
+        "artist": "Stevie Wonder",
+        "title": "Innervisions",
+        "releaseDate": 1973,
+        "cover": "https://images-na.ssl-images-amazon.com/images/I/51tsY%2B2a7fL.jpg"
     }
 ];
+
+const chooseAlbum = () => {
+    const albumNum = Math.floor(Math.random() * albumsList.length);
+
+    albumCover.innerHTML = `<img src='${albumsList[albumNum].cover}'>`;
+    albumTitle.innerText = albumsList[albumNum].title;
+    artist.innerText = albumsList[albumNum].artist;
+    releaseDate.innerText = albumsList[albumNum].releaseDate;
+
+};
+
+chooseButton.addEventListener("click", chooseAlbum);
